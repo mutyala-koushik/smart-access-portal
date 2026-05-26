03633ce366be34d69a3b721bc2058ace8d85144e
Mutyala Koushik <rohithmutyala2005@gmail.com>
2026-05-27 01:09:34 +0530

    Fixed model filename case issue

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -2,7 +2,7 @@ const express=require('express');
 
 const router=express.Router();
 
-const User=require('../models/User');
+const User=require('../models/user');
 
 const authMiddleware=require('../middleware/authMiddleware');
 
