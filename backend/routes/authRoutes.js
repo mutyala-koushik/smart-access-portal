6efa414e0018c29fa22c2fc442d8678cf06bb4cb
mutyala-koushik <rohithmutyala2005@gmail.com>
2026-05-28 14:23:32 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const User = require('../models/User');
+const User = require('../models/user');
 
 router.post('/login', async (req, res) => {
 
