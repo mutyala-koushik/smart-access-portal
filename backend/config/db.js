1cf1d320c0eda143f31508e1c3c85416806a376d
mutyala-koushik <rohithmutyala2005@gmail.com>
2026-05-28 13:52:04 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,19 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-
   try {
 
     await mongoose.connect(
-      'mongodb+srv://admin1:cQnIvf9BTxZ2g47b@cluster0.6rhxpd3.mongodb.net/smart-access-portal?retryWrites=true&w=majority'
+      'mongodb+srv://admin1:cQnIvf9BTxZ2g47b@cluster0.6rhxpd3.mongodb.net/smart-access-portal?retryWrites=true&w=majority&appName=Cluster0'
     );
 
     console.log('MongoDB Connected 😎');
 
   } catch (error) {
 
-    console.log('Mongo Error:', error);
+    console.log(error);
 
   }
-
 };
 
 module.exports = connectDB;
