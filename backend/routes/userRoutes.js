a8835b734c96f853b1bc6712792553404339839c
mutyala-koushik <rohithmutyala2005@gmail.com>
2026-05-28 13:13:48 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
+
 const User = require('../models/user');
 
 router.get('/users', async (req, res) => {
+
   try {
 
     const users = await User.find();
@@ -26,11 +29,14 @@ router.get('/users', async (req, res) => {
 
   } catch (error) {
 
+    console.log(error);
+
     res.status(500).json({
       message: error.message
     });
 
   }
+
 });
 
 module.exports = router;
